336770ed4a0e4995e00f0149b4aab760ff706484
Harley Coughlin <104121493+Cthuloops@users.noreply.github.com>
2025-07-19 13:41:56 -0400

    .

--- main.go
+++ main.go
@@ -1,14 +1,65 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main() {
-	newMux := http.NewServeMux()
+	apiCfg := apiConfig{}
+	mux := http.NewServeMux()
+
+	mux.Handle("/app/", http.StripPrefix("/app/",
+		apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
+
+	mux.Handle("/app/assets/", http.StripPrefix("/app/assets/",
+		http.FileServer(http.Dir("./assets"))))
+
+	mux.HandleFunc("GET /healthz", readiness)
+
+	mux.HandleFunc("GET /metrics", apiCfg.serveMetrics)
+	mux.HandleFunc("POST /reset", apiCfg.reset)
+
 	server := http.Server{
 		Addr:    ":8080",
-		Handler: newMux,
+		Handler: mux,
 	}
-	server.ListenAndServe()
+	log.Fatal(server.ListenAndServe())
+}
+
+func readiness(writer http.ResponseWriter, reader *http.Request) {
+	_ = reader
+	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) serveMetrics(writer http.ResponseWriter,
+	reader *http.Request) {
+	_ = reader
+	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(http.StatusOK)
+
+	hits := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
+	writer.Write([]byte(hits))
+}
+
+func (cfg *apiConfig) reset(writer http.ResponseWriter,
+	reader *http.Request) {
+	_ = reader
+	writer.WriteHeader(http.StatusOK)
+	cfg.fileserverHits.Store(0)
 }
